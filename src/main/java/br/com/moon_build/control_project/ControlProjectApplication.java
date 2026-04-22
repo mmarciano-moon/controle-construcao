80eb8cdc7ecda57641baa27aebd9fe6f5b419b24
mmarciano-moon <michel@moonpayment.com.br>
2026-04-21 21:02:23 -0300

    Initialize project with Maven setup, including wrapper scripts, POM file, and basic application structure

---
+++ src/main/java/br/com/moon_build/control_project/ControlProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.moon_build.control_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/moon_build/control_project/ControlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moon_build.control_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
